1e329943dd0ee124f53f6fa61deca9ef5938d8a8
Huy2304 <116505781+Huy2304@users.noreply.github.com>
2025-04-22 19:51:46 +0700

    .

--- src/main/kotlin/net/jobsearchapplication_api/data/service/devicetoken/DeviceTokenServiceImpl.kt
+++ src/main/kotlin/net/jobsearchapplication_api/data/service/devicetoken/DeviceTokenServiceImpl.kt
@@ -1,18 +1,13 @@
 package net.jobsearchapplication_api.data.service.devicetoken
 
-import net.jobsearchapplication_api.data.db.DatabaseFactory
 import net.jobsearchapplication_api.data.db.DatabaseFactory.dbQuery
-import net.jobsearchapplication_api.data.db.extensions.toDeviceTokens
-import net.jobsearchapplication_api.data.db.extensions.toJob
-import net.jobsearchapplication_api.data.db.schemas.CompanyTable
 import net.jobsearchapplication_api.data.db.schemas.DeviceTokenTable
-import net.jobsearchapplication_api.data.db.schemas.JobTable
 import net.jobsearchapplication_api.data.models.DeviceToken
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
-import org.jetbrains.exposed.sql.statements.InsertStatement
+import org.jetbrains.exposed.sql.update
 
-class DeviceTokenServiceImpl: DeviceTokenService {
+class DeviceTokenServiceImpl : DeviceTokenService {
 	override suspend fun getToken(id: String): DeviceToken? {
 		return dbQuery {
 			DeviceTokenTable.select { DeviceTokenTable.id eq id }.firstOrNull()?.let {
@@ -24,14 +19,21 @@ class DeviceTokenServiceImpl: DeviceTokenService {
 		}
 	}
 
-	override suspend fun createToken(id: String, params:String): DeviceToken? {
-		var statement: InsertStatement<Number>? = null
-		DatabaseFactory.dbQuery {
-			statement = DeviceTokenTable.insert {
-				it[DeviceTokenTable.id] = id
-				it[DeviceTokenTable.token] = params
+	override suspend fun createToken(id: String, params: String): DeviceToken? {
+		return dbQuery {
+			val existingToken = DeviceTokenTable.select { DeviceTokenTable.id eq id }.firstOrNull()
+			if (existingToken != null) {
+				DeviceTokenTable.update({ DeviceTokenTable.id eq id }) {
+					it[token] = params
+				}
+				DeviceToken(id, params)
+			} else {
+				DeviceTokenTable.insert {
+					it[DeviceTokenTable.id] = id
+					it[DeviceTokenTable.token] = params
+				}
+				DeviceToken(id, params)
 			}
 		}
-		return statement?.resultedValues?.get(0)?.toDeviceTokens()
 	}
 }
